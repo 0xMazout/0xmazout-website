1a840f5f69b0ec124181d71fd01248e315ad8f6b
0xMazout <lucas.fabre.freelance@gmail.com>
2023-11-26 17:03:37 +0100

    feat: adding socials svg and links

--- app/components/ui/LeftSide/Socials.tsx
+++ app/components/ui/LeftSide/Socials.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import Image from "next/image";
+import Github from "@/public/logos/Github";
+import Linkedin from "@/public/logos/Linkedin";
+import Twitter from "@/public/logos/Twitter";
 
-type Props = {};
-
-const Socials = (props: Props) => {
+const Socials = () => {
   return (
-    <div>
-      <Image src={""} alt={""} />
-      Github Linkedin Twitter Logo Pulse : Linkedin de Pulse Logo Oxyzon :
-      Linkedin de Oxyzon Logo AC : Twitter Altier Capital
+    <div className="flex gap-5">
+      <Github className="fill-slate-500 hover:fill-white w-5 h-5 cursor-pointer" />
+      <Linkedin className="fill-slate-500 hover:fill-white w-5 h-5 cursor-pointer" />
+      <Twitter className="fill-slate-500 hover:fill-white w-5 h-5 cursor-pointer" />
+      {/* Github Linkedin Twitter Logo Pulse : Linkedin de Pulse Logo Oxyzon :
+      Linkedin de Oxyzon Logo AC : Twitter Altier Capital */}
     </div>
   );
 };
