52345c59acc16f68b01021d8732b800fdc30cfef
0xMazout <lucas.fabre.freelance@gmail.com>
2023-11-27 13:32:26 +0100

    fix:svgs - best way was to put directly svg into the file feat: install svgr fix: clean code

--- app/components/ui/Experiences/ExperienceCard.tsx
+++ app/components/ui/Experiences/ExperienceCard.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { RightCornerArrow } from "@/public/icons/rightCornerArrow";
-import Link from "next/link";
-import { ChainLink } from "@/public/icons/chainLink";
-import { Experience } from "../../../types/types";
 
+import Link from "next/link";
+import { ChainLink } from "@/public/icons/components/ChainLink";
+import { Experience } from "@/app/types/types";
+s;
 const ExperienceCard = ({
   role,
   company,
@@ -55,7 +55,7 @@ const ExperienceCard = ({
   };
 
   return (
-    <div className="group relative grid pb-1 grid-cols-8 transition-all gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+    <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
       <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
       <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
         {startDate} — {endDate}
@@ -63,14 +63,27 @@ const ExperienceCard = ({
       <div className="z-10 col-span-6">
         <h3 className="font-medium leading-snug text-slate-200">
           <Link
-            className="items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+            className="items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group-hover:text-teal-300  group/link text-base"
             href={companyLink}
           >
-            <h5 className="inline-block group">
+            <h5 className="inline-block">
               {role}
               {" @ "}
               {company}
-              <RightCornerArrow />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                width="20"
+                className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
             </h5>
           </Link>
         </h3>
