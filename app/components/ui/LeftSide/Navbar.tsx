066f4bc7b3285b640065e737c238310f8505bd42
0xMazout <lucas.fabre.freelance@gmail.com>
2023-11-29 17:40:20 +0100

    feat: navbar is working

--- app/components/ui/LeftSide/Navbar.tsx
+++ app/components/ui/LeftSide/Navbar.tsx
@@ -1,30 +1,114 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect, useCallback } from "react";
+import { useDebounce } from "usehooks-ts";
+import { cn } from "../../../utils/utils";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const [activeSection, setActiveSection] = useState("experience");
+
+  const handleIntersection = (entries: any[], observer: any) => {
+    entries.forEach(
+      (entry: {
+        isIntersecting: any;
+        target: { id: React.SetStateAction<string> };
+      }) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id);
+        }
+      },
+    );
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      rootMargin: "0px",
+      threshold: 0.5,
+    });
+
+    const sections = ["about", "experience", "projects"];
+    sections.forEach((section) => {
+      const element = document.getElementById(section);
+      if (element) {
+        observer.observe(element);
+      }
+    });
+
+    return () => {
+      sections.forEach((section) => {
+        const element = document.getElementById(section);
+        if (element) {
+          observer.unobserve(element);
+        }
+      });
+    };
+  }, []);
+
   return (
     <ul className="mt-16 w-max">
       <li>
-        <a className="group flex items-center py-3 active" href="#about">
-          <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-          <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+        <a
+          className={"group flex items-center py-3"}
+          href="#about"
+          onClick={() => setActiveSection("about")}
+        >
+          <span
+            className={cn(
+              "nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 motion-reduce:transition-none",
+              activeSection == "about" && "w-16 bg-slate-200",
+            )}
+          ></span>
+          <span
+            className={cn(
+              "nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200",
+              activeSection == "about" && " text-slate-200",
+            )}
+          >
             About
           </span>
         </a>
       </li>
       <li>
-        <a className="group flex items-center py-3" href="#experience">
-          <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-          <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+        <a
+          className={`group flex items-center py-3`}
+          href="#experience"
+          onClick={() => setActiveSection("experience")}
+        >
+          <span
+            className={cn(
+              "nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 motion-reduce:transition-none",
+              activeSection == "experience" && "w-16 bg-slate-200",
+            )}
+          ></span>
+          <span
+            className={cn(
+              "nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200",
+              activeSection == "experience" && " text-slate-200",
+            )}
+          >
             Experience
           </span>
         </a>
       </li>
       <li>
-        <a className="group flex items-center py-3" href="#projects">
-          <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-          <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+        <a
+          className={`group flex items-center py-3 `}
+          href="#projects"
+          onClick={() => setActiveSection("projects")}
+        >
+          <span
+            className={cn(
+              "nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 motion-reduce:transition-none",
+              activeSection == "projects" && "w-16 bg-slate-200",
+            )}
+          ></span>
+          <span
+            className={cn(
+              "nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200",
+              activeSection == "projects" && " text-slate-200",
+            )}
+          >
             Projects
           </span>
         </a>
--- app/components/ui/RightSide/About.tsx
+++ app/components/ui/RightSide/About.tsx
@@ -5,7 +5,10 @@ type Props = {};
 
 const About = (props: Props) => {
   return (
-    <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 max-w-2xl">
+    <section
+      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24 max-w-2xl"
+      id="about"
+    >
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2
           id="about"
